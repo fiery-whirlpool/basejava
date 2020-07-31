98e4d1e1b524ad10a6f97723dc14ab778d1d116b
Inzin <mr.temka-rvdoz@yandex.ru>
2020-07-31 20:27:18 +0300

    тестовое задание

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,26 +5,79 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
     void save(Resume r) {
+        for(int i=0;i<storage.length;i++){
+            if (storage[i]==null){
+                storage[i]= r;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
-    }
+        int idx=0;
+        for(int i=0; i<storage.length;i++) {
+            if(storage[i] != null) {
+                if(storage[i].uuid == uuid){
+                    idx = i;
+                    break;
+                }
+            }else
+                break;
+        }
+         return storage[idx];
+     }
+
 
     void delete(String uuid) {
+        int count = 0;
+        for(int i=0; i<storage.length;i++) {
+            if (storage[i] != null) {
+                count ++;
+                if (storage[i].uuid == uuid) {
+                    for (int j = i; j < storage.length; j++) {
+                        if (storage[j] != null && storage[j + 1] != null) {
+                            storage[j] = storage[j + 1];
+                        }
+                    }
+                }
+            }
+        }
+        storage[count-1]=null;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int count=0;
+        for(int i=0; i<storage.length; i++){
+            if(storage[i] != null){
+              count ++;
+            }
+        }
+        Resume[] res = new Resume[count];
+        for(int i=0; i<count;i++){
+            res[i] = storage[i];
+        }
+        return res;
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                count++;
+            } else break;
+        }
+        return count;
+
     }
 }
